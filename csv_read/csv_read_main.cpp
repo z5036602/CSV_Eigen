ed60f287c7ee4e4185e841970e7bed5d345adfa0
z5036602 <joshua19970320@gmail.com>
2022-07-06 13:19:44 +1000

    Jul 6
    CSV_file pricessing in c++

---
+++ csv_read/csv_read_main.cpp
@@ -0,0 +1,29 @@
+#include <Eigen/Dense>
+#include <vector>
+#include <fstream>
+#include <iostream>
+using namespace Eigen;
+
+template<typename M>
+M load_csv (const std::string & path) {
+    std::ifstream indata;
+    indata.open(path);
+    std::string line;
+    std::vector<double> values;
+    uint rows = 0;
+    while (std::getline(indata, line)) {
+        std::stringstream lineStream(line);
+        std::string cell;
+        while (std::getline(lineStream, cell, ',')) {
+            values.push_back(std::stod(cell));
+        }
+        ++rows;
+    }
+    return Map<const Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime, RowMajor>>(values.data(), rows, values.size()/rows);
+}
+int main()
+{
+    MatrixXd A = load_csv<MatrixXd>("IMU_data.csv");
+    std::cout << A << std::endl;
+
+}
